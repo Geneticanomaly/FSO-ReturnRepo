d4404f9e26a96ba0b72be2695ab9f186925f2af3
Geneticanomaly <thomaskaatranen@gmail.com>
2024-06-18 16:25:32 +0300

    Addition of task 1.6 unicafe step1

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({handleClick, text}) => {
+    return <button onClick={() => handleClick(text)}>{text}</button>;
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-export default App
+    const handleClick = (text) => {
+        if (text === 'good') {
+            setGood(good + 1);
+        } else if (text === 'neutral') {
+            setNeutral(neutral + 1);
+        } else {
+            setBad(bad + 1);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button handleClick={handleClick} text="good" />
+            <Button handleClick={handleClick} text="neutral" />
+            <Button handleClick={handleClick} text="bad" />
+            <h1>Statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </div>
+    );
+};
+
+export default App;
