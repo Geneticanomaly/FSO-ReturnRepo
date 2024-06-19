70d330d2cc9f8378f1e20e9ca9db1bed7538177b
Geneticanomaly <thomaskaatranen@gmail.com>
2024-06-19 21:57:26 +0300

    Addition of task 2.6 puhelinluettelo step1.

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,40 @@
+import {useState} from 'react';
+
 const App = () => {
-    return <div>App</div>;
+    const [persons, setPersons] = useState([{name: 'Arto Hellas'}]);
+    const [newName, setNewName] = useState('');
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setNewName(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newPerson = {
+            name: newName,
+        };
+        setPersons(persons.concat(newPerson));
+        setNewName('');
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <div>
+                    name: <input onChange={(e) => handleChange(e)} value={newName} />
+                </div>
+                <div>
+                    <button type="submit">Add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((person, i) => (
+                <p key={i}>{person.name}</p>
+            ))}
+        </div>
+    );
 };
 
 export default App;
