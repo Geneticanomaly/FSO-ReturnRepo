de98d7f76c5ea37eb50ecdef5d1409dea5634796
Geneticanomaly <thomaskaatranen@gmail.com>
2024-06-24 21:04:03 +0300

    Addition of task 2.14 puhelinluettelo step10.

--- osa2/puhelinluettelo/src/components/Persons.jsx
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -1,11 +1,21 @@
+import personService from '../services/persons';
+
 const Persons = ({persons, filter}) => {
     const filteredPersons = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()));
 
+    const deletePerson = (id) => {
+        const personToDelete = persons.find((person) => person.id === id);
+        if (window.confirm(`Delete ${personToDelete.name}?`)) {
+            personService.deletePerson(id);
+        }
+    };
+
     return (
         <div>
-            {filteredPersons.map((person, i) => (
-                <p key={i}>
+            {filteredPersons.map((person) => (
+                <p key={person.id}>
                     {person.name} {person.number}
+                    <button onClick={() => deletePerson(person.id)}>Delete</button>
                 </p>
             ))}
         </div>
