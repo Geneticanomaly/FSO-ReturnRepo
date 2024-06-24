bafc2f598c29989e658d780f2ee93f0c762d27c8
Geneticanomaly <thomaskaatranen@gmail.com>
2024-06-24 19:44:24 +0300

    Addition of task 2.11 puhelinluettelo step6.

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,15 +1,11 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456'},
-        {name: 'Ada Lovelace', number: '39-44-5323523'},
-        {name: 'Dan Abramov', number: '12-43-234345'},
-        {name: 'Mary Poppendieck', number: '39-23-6423122'},
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [number, setNumber] = useState('');
     const [filter, setFilter] = useState('');
@@ -44,6 +40,15 @@ const App = () => {
             setNumber('');
         }
     };
+
+    useEffect(() => {
+        const fetchPersonsData = () => {
+            axios.get('http://localhost:3001/persons').then((res) => {
+                setPersons(res.data);
+            });
+        };
+        fetchPersonsData();
+    }, []);
 
     return (
         <div>
