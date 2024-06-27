af735f62fd0c5e8b55fb36c0456115d9413e8181
Geneticanomaly <thomaskaatranen@gmail.com>
2024-06-27 18:23:36 +0300

    Fixed an issue regarding to the displaying of the weather icon.

--- osa2/maidenTiedot/src/components/CityWeather.jsx
+++ osa2/maidenTiedot/src/components/CityWeather.jsx
@@ -1,11 +1,10 @@
 const CityWeather = ({weather}) => {
-    console.log('HUH');
     const celsius = Math.ceil((weather.main.temp - 273.15) * 10) / 10;
     return (
         <div>
             <h2>Weather in {weather.name}</h2>
             <p>Temperature {celsius} Celsius</p>
-            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].description}2x.png`} />
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
             <p>Wind {weather.wind.speed} m/s</p>
         </div>
     );
--- osa2/maidenTiedot/src/services/Weather.js
+++ osa2/maidenTiedot/src/services/Weather.js
@@ -3,7 +3,6 @@ const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const api_key = import.meta.env.VITE_SOME_KEY;
 
 const getCityWeather = (city) => {
-    console.log('fetching');
     const req = axios.get(`${baseUrl}${city}&appid=${api_key}`);
     return req.then((res) => res.data);
 };
