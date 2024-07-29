8f75121120cbbb4a29ac32f709840aaa26f7de4b
Geneticanomaly <thomaskaatranen@gmail.com>
2024-07-29 10:40:44 +0300

    Created a app.js file that now is in charge of express. Index.js is now only in charge of handling application start and the port.

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,25 +1,5 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const app = require('./app');
 const config = require('./utils/config');
-const blogsRouter = require('./controllers/blogs');
-
-mongoose.set('strictQuery', false);
-
-mongoose
-    .connect(config.MONGODB_URI)
-    .then((result) => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message);
-    });
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`);
