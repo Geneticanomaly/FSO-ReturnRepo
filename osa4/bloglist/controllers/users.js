65b512a94280c22cdd220c447759784cc6f944aa
Geneticanomaly <thomaskaatranen@gmail.com>
2024-07-30 20:27:30 +0300

    Addition of task 4.16 Blog List Expansion, step 4.

--- osa4/bloglist/controllers/users.js
+++ osa4/bloglist/controllers/users.js
@@ -10,13 +10,17 @@ usersRouter.get('/', async (req, res) => {
 usersRouter.post('/', async (req, res) => {
     const {username, name, password} = req.body;
 
+    if (!password || password.length < 3) {
+        return res.status(400).json({error: 'invalid password, need to be atleast 3 characters long'});
+    }
+
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(password, salt);
 
     const user = new User({
-        username,
-        name,
-        passwordHash,
+        username: username,
+        name: name,
+        password: passwordHash,
     });
 
     const savedUser = await user.save();
