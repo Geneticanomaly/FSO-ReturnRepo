81132929acef346847e51f7032c0bdddd91a9a1b
Geneticanomaly <thomaskaatranen@gmail.com>
2024-08-01 18:47:37 +0300

    Addition of task 4.22 Blog List Expansion, step 10

--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -1,44 +1,32 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
+const middleware = require('../utils/middleware');
 
 blogsRouter.get('/', async (req, res) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1});
     res.json(blogs);
 });
 
-blogsRouter.post('/', async (req, res) => {
+blogsRouter.post('/', middleware.userExtractor, async (req, res) => {
     const likeAmount = req.body.likes ? req.body.likes : 0;
-
-    const decodedToken = jwt.verify(req.token, process.env.SECRET);
-    if (!decodedToken.id) {
-        return res.status(401).json({error: 'token invalid'});
-    }
-    const user = await User.findById(decodedToken.id);
 
     const blog = new Blog({
         title: req.body.title,
         author: req.body.author,
         url: req.body.url,
         likes: likeAmount,
-        user: user._id,
+        user: req.user._id,
     });
 
     const savedBlog = await blog.save();
-    user.blogs = user.blogs.concat(savedBlog._id);
-    await user.save();
+    req.user.blogs = req.user.blogs.concat(savedBlog._id);
+    await req.user.save();
 
     res.status(201).json(savedBlog);
 });
 
-blogsRouter.delete('/:id', async (req, res) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (req, res) => {
     const id = req.params.id;
-
-    const decodedToken = jwt.verify(req.token, process.env.SECRET);
-    if (!decodedToken.id) {
-        return res.status(401).json({error: 'token invalid'});
-    }
 
     const blog = await Blog.findById(id);
 
@@ -46,7 +34,7 @@ blogsRouter.delete('/:id', async (req, res) => {
         return res.status(404).json({error: 'no such blog found'});
     }
 
-    if (blog.user.toString() !== decodedToken.id) {
+    if (blog.user.toString() !== req.user.id) {
         return res.status(403).json({error: 'action not authorized'});
     }
 
