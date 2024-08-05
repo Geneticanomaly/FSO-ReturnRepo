5bbc876f2350922fd39b17fcc4cfb5d66fe820a6
Geneticanomaly <thomaskaatranen@gmail.com>
2024-08-05 16:32:58 +0300

    Addition of task 5.7 Blog List Frontend, step 7.

--- osa4/bloglist-frontend/src/components/Blog.jsx
+++ osa4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from 'react';
+import '../index.css';
 
-export default Blog
+const Blog = ({blog}) => {
+    const [isVisible, setVisible] = useState(false);
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+    };
+
+    console.log(blog);
+
+    const handleClick = () => {
+        setVisible(!isVisible);
+    };
+
+    return (
+        <div style={blogStyle}>
+            <div>
+                {blog.title} {blog.author}
+                <button onClick={handleClick}>{!isVisible ? 'view' : 'hide'}</button>
+                {isVisible && (
+                    <div className="blog-content">
+                        <p>{blog.url}</p>
+                        <div className="blog-likes">
+                            <p>likes {blog.likes}</p> <button>like</button>
+                        </div>
+                        <p>{blog.user.name}</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
