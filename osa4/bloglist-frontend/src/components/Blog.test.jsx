8578317cfc44fbeb0477087f0f05261cc0bd0ee6
Geneticanomaly <thomaskaatranen@gmail.com>
2024-08-06 16:42:38 +0300

    Addition of task 5.14 Blog List Tests, step 2.

--- osa4/bloglist-frontend/src/components/Blog.test.jsx
+++ osa4/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 import Notification from './Notification';
 
@@ -13,4 +14,44 @@ test('renders content', () => {
 
     const element = screen.getByText(/The Two Reacts/i);
     expect(element).toBeDefined();
+});
+
+test('All blog info is showcased when view button is pressed', async () => {
+    const user = {
+        token: '',
+        username: 'hellas',
+        name: 'Arto Hellas',
+    };
+    const blog = {
+        title: 'The Two Reacts',
+        author: 'Dan Abramov',
+        url: 'https://overreacted.io/things-i-dont-know-as-of-2018/',
+        likes: 0,
+        user: {
+            username: 'hellas',
+            name: 'Arto Hellas',
+            id: '66aa7132dd35770081cf1b9f',
+        },
+    };
+
+    render(<Blog blog={blog} user={user} />);
+
+    const title = screen.getByText(/The Two Reacts/i);
+    expect(title).toBeDefined();
+
+    const author = screen.getByText(/Dan Abramov/i);
+    expect(author).toBeDefined();
+
+    const event = userEvent.setup();
+    const button = screen.getByText(/view/i);
+    await event.click(button);
+
+    const url = screen.getByText(/https:\/\/overreacted\.io\/things-i-dont-know-as-of-2018\//i);
+    expect(url).toBeDefined();
+
+    const likes = screen.getByText(/likes 0/i);
+    expect(likes).toBeDefined();
+
+    const publisher = screen.getByText(/Arto Hellas/i);
+    expect(publisher).toBeDefined();
 });
