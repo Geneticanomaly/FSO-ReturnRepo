f06dd4baffb3572fd22be468629e82128763eb8f
Geneticanomaly <thomaskaatranen@gmail.com>
2024-08-06 17:14:59 +0300

    Addition of task 5.15 Blog List Tests, step 3.

--- osa4/bloglist-frontend/src/components/Blog.test.jsx
+++ osa4/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,15 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
-import Notification from './Notification';
+
+const user = {
+    token: '',
+    username: 'hellas',
+    name: 'Arto Hellas',
+};
+const blog = {
+    title: 'The Two Reacts',
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/things-i-dont-know-as-of-2018/',
+    likes: 0,
+    user: {
+        username: 'hellas',
+        name: 'Arto Hellas',
+        id: '66aa7132dd35770081cf1b9f',
+    },
+};
 
 test('renders content', () => {
-    const blog = {
-        title: 'The Two Reacts',
-        author: 'Dan Abramov',
-        url: 'https://overreacted.io/things-i-dont-know-as-of-2018/',
-    };
-
     render(<Blog blog={blog} />);
 
     const element = screen.getByText(/The Two Reacts/i);
@@ -17,23 +27,6 @@ test('renders content', () => {
 });
 
 test('All blog info is showcased when view button is pressed', async () => {
-    const user = {
-        token: '',
-        username: 'hellas',
-        name: 'Arto Hellas',
-    };
-    const blog = {
-        title: 'The Two Reacts',
-        author: 'Dan Abramov',
-        url: 'https://overreacted.io/things-i-dont-know-as-of-2018/',
-        likes: 0,
-        user: {
-            username: 'hellas',
-            name: 'Arto Hellas',
-            id: '66aa7132dd35770081cf1b9f',
-        },
-    };
-
     render(<Blog blog={blog} user={user} />);
 
     const title = screen.getByText(/The Two Reacts/i);
@@ -54,4 +47,21 @@ test('All blog info is showcased when view button is pressed', async () => {
 
     const publisher = screen.getByText(/Arto Hellas/i);
     expect(publisher).toBeDefined();
+});
+
+test('like button calls a function when pressed', async () => {
+    const mockHandler = vi.fn();
+    render(<Blog blog={blog} user={user} updateBlog={mockHandler} />);
+
+    const event = userEvent.setup();
+    const button = screen.getByText(/view/i);
+    await event.click(button);
+
+    const likeButton = screen.getByRole('button', { name: /like/i });
+    expect(likeButton).toBeDefined();
+
+    await event.click(likeButton);
+    await event.click(likeButton);
+
+    expect(mockHandler.mock.calls).toHaveLength(2);
 });
