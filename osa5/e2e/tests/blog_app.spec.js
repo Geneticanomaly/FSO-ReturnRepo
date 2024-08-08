337a9eb61b968fe77cf4059fa6174df127427f15
Geneticanomaly <thomaskaatranen@gmail.com>
2024-08-08 13:44:09 +0300

    Addition of task 5.18 Blog List e2e Tests, step 2.

--- osa5/e2e/tests/blog_app.spec.js
+++ osa5/e2e/tests/blog_app.spec.js
@@ -1,8 +1,15 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
         await page.goto('http://localhost:5173/');
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                name: 'Matti Luukkainen',
+                username: 'mluukkai',
+                password: 'salainen',
+            },
+        });
     });
 
     test('Login form is displayed', async ({ page, request }) => {
@@ -17,5 +24,22 @@ describe('Blog app', () => {
 
         const button = page.getByRole('button', { name: 'login' });
         await expect(button).toBeVisible();
+    });
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByRole('textbox').first().fill('mluukkai');
+            await page.getByRole('textbox').last().fill('salainen');
+            await page.getByRole('button', { name: 'login' }).click();
+
+            await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible();
+        });
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByRole('textbox').first().fill('dannylee');
+            await page.getByRole('textbox').last().fill('wrong');
+            await page.getByRole('button', { name: 'login' }).click();
+
+            await expect(page.getByText('invalid username or password')).toBeVisible();
+        });
     });
 });
