ce276bbd9e473f82ae492f20dfa491a9609802d5
Geneticanomaly <thomaskaatranen@gmail.com>
2024-08-20 16:08:26 +0300

    Addition of task 8.7 Updating the birth year of an author.

--- osa8/graphql-backend/library-backend.js
+++ osa8/graphql-backend/library-backend.js
@@ -113,7 +113,11 @@ const typeDefs = `
         author: String!
         published: Int!
         genres: [String!]!
-    ) : Book
+    ) : Book,
+     editAuthor(
+        name: String!
+        setBornTo: Int!
+     ) : Author
   }
 `;
 
@@ -149,6 +153,17 @@ const resolvers = {
             const book = { ...args, id: uuid() };
             books = books.concat(book);
             return book;
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find((author) => author.name === args.name);
+
+            if (!author) return null;
+
+            const updatedAuthor = { ...author, born: args.setBornTo };
+
+            authors = authors.map((author) => (author.name !== args.name ? author : updatedAuthor));
+
+            return updatedAuthor;
         },
     },
     Author: {
