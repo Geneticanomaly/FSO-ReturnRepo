6190089590e29c735e46d3ddcc75ada7634ae417
Geneticanomaly <thomaskaatranen@gmail.com>
2024-08-21 16:10:58 +0300

    Addition of task 8.12 Authors birth year advanced.

--- osa8/library-frontend/src/components/BirthYearForm.jsx
+++ osa8/library-frontend/src/components/BirthYearForm.jsx
@@ -1,44 +1,50 @@
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries';
+import Select from 'react-select';
 
-const BirthYearForm = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        born: '',
-    });
+const BirthYearForm = ({ authors }) => {
+    const [name, setName] = useState('');
+    const [bornDate, setBornDate] = useState('');
 
     const [editAuthor] = useMutation(EDIT_AUTHOR, {
         refetchQueries: [{ query: ALL_AUTHORS }],
     });
 
-    const handleChange = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }));
-    };
+    const options = authors.map((author) => ({
+        value: author.name,
+        label: author.name,
+    }));
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const bornToNumber = Number(formData.born);
-        editAuthor({ variables: { name: formData.name, setBornTo: bornToNumber } });
 
-        setFormData({
-            name: '',
-            born: '',
-        });
+        const bornToNumber = Number(bornDate);
+        editAuthor({ variables: { name: name, setBornTo: bornToNumber } });
+
+        setName('');
+        setBornDate('');
     };
 
     return (
         <div>
             <h2>Set birthYear</h2>
             <form onSubmit={handleSubmit}>
+                <Select
+                    name="name"
+                    options={options}
+                    value={options.find((option) => option.value === name) || ''}
+                    onChange={(e) => setName(e.value)}
+                    placeholder="Select an author"
+                />
                 <div>
-                    name <input type="text" name="name" onChange={handleChange} />
-                </div>
-                <div>
-                    born <input type="number" name="born" onChange={handleChange} />
+                    born{' '}
+                    <input
+                        type="number"
+                        name="born"
+                        value={bornDate}
+                        onChange={(e) => setBornDate(e.target.value)}
+                    />
                 </div>
                 <button>update author</button>
             </form>
